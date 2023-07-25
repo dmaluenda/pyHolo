6a1f06fdd80d4d5f2a3a29b9e6898ee4f2eebace
David Maluenda Niubó <dmaluenda@ub.edu>
2023-07-25 17:02:15 +0200

    Cleaning code from matlab and old labview

--- gui/SupportFiles/SemiGrayLevels/semilevel.m
+++
@@ -1,5 +0,0 @@
-im=zeros(768,1024)*256;
-for i=0:255
-    im(1:1024*768/2)=i;
-    imwrite(im/255,[num2str(i) 'semilevel.png'],'PNG');
-end
--- gui/SupportFiles/scripts/SwitcherOff.m
+++
@@ -1,5 +0,0 @@
-for display=1:2;
-hologram=mapa_Holo3(0,0,[768 1024],display);
-imwrite(hologram,['SwitchOff_' num2str(display) '.bmp'],'BMP')
-%imshow(hologram)
-end
